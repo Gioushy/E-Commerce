7ac2c424334568e4a0e02ce2d248e38b23247c7f
Mustafa Elgioushy <105521854+Gioushy@users.noreply.github.com>
2024-05-14 19:07:58 +0300

    Update sample.sql

--- sample.sql
+++ sample.sql
@@ -1,2 +1,5 @@
 -- SQL query to generate a daily report of the total revenue for a specific date, where the placeholder is the date (?)
-SELECT SUM(total_amount) FROM order WHERE date = ? ;
+SELECT SUM(total_amount) FROM order WHERE order_date = ? ;
+
+-- SQL query to generate monthly report of the top-selling products in a given month, where the placeholder is the month(?)
+SELECT p.id,
