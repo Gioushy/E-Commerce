3815240f46ed1942875fe81809f5023bb8e98fbe
Mustafa Elgioushy <105521854+Gioushy@users.noreply.github.com>
2024-05-19 15:54:45 +0300

    Update ddl.sql

--- ddl.sql
+++ ddl.sql
@@ -1,8 +1,8 @@
 -- Creating the category table
 CREATE TABLE category (
-	"id" UUID DEFAULT gen_random_uuid(),
-	"name" VARCHAR(50) NOT NULL,
-	subcategory_id UUID,
+	"id" INT SERIAL,
+	"name" VARCHAR(150) NOT NULL,
+	subcategory_id INT,
 	PRIMARY KEY ("id"),
 	CONSTRAINT fk_subcategory
 		FOREIGN KEY (subcategory_id)
@@ -11,17 +11,17 @@ CREATE TABLE category (
 );
 -- Creating product table
 CREATE TABLE product (
-	"id" UUID DEFAULT gen_random_uuid(),
-	"name" VARCHAR(50) NOT NULL,
-	description VARCHAR(150) NOT NULL,
+	"id" INT SERIAL,
+	"name" VARCHAR(100) NOT NULL,
+	description TEXT NOT NULL,
 	price DECIMAL(9,2) NOT NULL,
-	stock_quantity SMALLINT,
+	stock_quantity SMALLINT NOT NULL,
 	PRIMARY KEY ("id")
 );
 -- Creating the associative entity between products and categories
 CREATE TABLE categoryproducts (
-	category_id UUID,
-	product_id UUID,
+	category_id INT,
+	product_id INT,
 	PRIMARY KEY(category_id,product_id),
 	CONSTRAINT fk_category_id
 		FOREIGN KEY(category_id)
@@ -32,20 +32,22 @@ CREATE TABLE categoryproducts (
 );
 -- Creating customer table
 CREATE TABLE customer(
-	"id" UUID DEFAULT gen_random_uuid(),
-	first_name VARCHAR(50) NOT NULL,
-	last_name VARCHAR(50) NOT NULL,
-	email VARCHAR(50) NOT NULL,
-	password VARCHAR(50) NOT NULL,
+	"id" INT SERIAL,
+	first_name VARCHAR(100) NOT NULL,
+	last_name VARCHAR(100) NOT NULL,
+	email VARCHAR(100) NOT NULL,
+	password VARCHAR(100) NOT NULL,
 	PRIMARY KEY("id"),
 	CONSTRAINT email_format
 		CHECK (email ~* '^[A-Za-z0-9._+%-]+@[A-Za-z0-9.-]+[.][A-Za-z]+$')
 );
 -- Creating order table
 CREATE TABLE "order"(
-	"id" UUID DEFAULT gen_random_uuid(),
-	customer_id UUID,
-	date DATE NOT NULL DEFAULT CURRENT_DATE,
+	"id" INT SERIAL,
+	customer_id INT,
+	order_date TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
+	shipped_date TIMESTAMP,
+	active BOOLEAN,
 	total_amount DECIMAL(9,2) NOT NULL,
 	PRIMARY KEY("id"),
 	CONSTRAINT fk_customer_id
@@ -54,11 +56,11 @@ CREATE TABLE "order"(
 );
 -- Creating the associative entity between order and product
 CREATE TABLE order_details(
-	"id" UUID DEFAULT gen_random_uuid(),
-	product_id UUID,
-	order_id UUID,
+	"id" INT SERIAL,
+	product_id INT,
+	order_id INT,
 	quantity SMALLINT NOT NULL,
-	unit_price DECIMAL(9,2),
+	unit_price DECIMAL(9,2) NOT NULL,
 	PRIMARY KEY("id"),
 	CONSTRAINT fk_product_id
 		FOREIGN KEY(product_id)
